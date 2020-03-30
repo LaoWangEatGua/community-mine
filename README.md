61eab2576238126e50cfaab28dcd51c9f24ab499
MrBin <872558639@qq.com>
2020-03-30 13:30:43 +0800

    extract github config from code to properties

---
+++ README.md
@@ -0,0 +1,7 @@
+#论坛小项目
+
+[Github OAuth](https://www.bilibili.com/video/BV1r4411r7au?p=8)
+##资料
+
+
+##工具
---
+++ src/main/java/com/mrbin/community/controller/AuthorizeController.java
@@ -0,0 +1,43 @@
+package com.mrbin.community.controller;
+
+import com.mrbin.community.dto.AccessTokenDTO;
+import com.mrbin.community.dto.GithubUser;
+import com.mrbin.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.Client.secret}")
+    private String clientSecret;
+
+    @Value("${github.Redirect.uri}")
+    private String redirectUri;
+
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam("code") String code,
+                           @RequestParam("state")String state){
+
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
